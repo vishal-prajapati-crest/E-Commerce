556f4c31ea3efa61f1dc433afec9961ba6e81549
vishal-prajapati-crest <vishal.prajapati@crestinfosystems.com>
2024-03-19 12:41:02 +0530

    Add logic for admin register, login & logout

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
     use HasFactory;
+    use HasApiTokens;
 
     public function user(): BelongsTo
     {
